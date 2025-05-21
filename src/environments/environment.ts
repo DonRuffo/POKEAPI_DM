fcb34f5c8cdb169df493984342b0b01f44b38def
FAMILIA <dennisdiaz407@gmail.com>
2025-05-20 22:26:46 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyA7gFoYgUOkXR85R4If_xY41WL59iGzQD4",
-    authDomain: "formsdennis.firebaseapp.com",
-    projectId: "formsdennis",
-    storageBucket: "formsdennis.firebasestorage.app",
-    messagingSenderId: "427741107203",
-    appId: "1:427741107203:web:9a779c3b2b578495272a36",
-    measurementId: "G-8WXSLDJYQJ"
-  }
+  production: false
 };
 
 /*
